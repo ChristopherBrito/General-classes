62833bf0d3df78c93625eaaef997412275aa76a5
ChristopherBrito <britc30@farmingdale.edu>
2025-04-15 10:09:14 -0400

    Add files via upload

---
+++ Main-3.java
@@ -0,0 +1,9 @@
+public class Main{
+	public static void main(String[] args) {
+		Point<String> strPoint = new Point<String>("Anna", "Banana");
+		System.out.println(strPoint);
+		Point<Number> pie = new Point<Number>(3.14, 2.17);
+		System.out.println(pie);
+	}
+}
+	
